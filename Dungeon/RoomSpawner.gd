e4fa265b666577a52d26d166f3decb3f4c39e8c0
Valor <vasszili@gmail.com>
2021-02-06 14:10:11 +0100

    This just looks better doesn't it?

--- Dungeon/RoomSpawner.gd
+++ Dungeon/RoomSpawner.gd
@@ -6,29 +6,31 @@ export var _opening_direction: int = 0
 
 func _ready() -> void:
 	set_process(false)
+
 	if get_tree().get_current_scene().rooms.has(global_transform.origin):
 		return
+
 	get_tree().get_current_scene().rooms.append(global_transform.origin)
+
 	randomize()
 	set_process(true)
 
 
 func _process(_delta: float) -> void:
+	# 
 	var _rooms: Array
-	var _random: int
 
 	match _opening_direction:
 		1:  # Spawn top room
 			_rooms = get_tree().get_current_scene().top_rooms
-			_random = randi() % _rooms.size()
 		2:  # Spawn bot room
 			_rooms = get_tree().get_current_scene().bot_rooms
-			_random = randi() % _rooms.size()
 		3:  # Spawn left room
 			_rooms = get_tree().get_current_scene().left_rooms
-			_random = randi() % _rooms.size()
 		4:  # Spawn right room
 			_rooms = get_tree().get_current_scene().right_rooms
-			_random = randi() % _rooms.size()
+
+	# Choose a random index of the rooms array.
+	var _random: int = randi() % _rooms.size()
 	add_child(load(_rooms[_random]).instance())
 	set_process(false)
